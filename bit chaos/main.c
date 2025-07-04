2e55788394c86e31ad06ca3d0d3ea3a51d3428ce
GodRay7 <andrey2008an@gmail.com>
2025-07-04 14:45:24 +0200

    gitignore fixed for all folders

--- bit chaos/main.c
+++ bit chaos/main.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "bmp.h"
+
+
+int main()
+{
+
+    return 0;
+}
---
+++ vigenere cipher/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "bmp.h"
+
+
+int main()
+{
+
+    return 0;
+}
