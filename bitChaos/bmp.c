fe3225b8fb6e4efbb1dba70d0b0741ab463b09fe
GodRay7 <andrey2008an@gmail.com>
2025-07-05 08:58:52 +0200

    2 folders renamed

---
+++ bitChaos/bmp.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "bmp.h"
+
+void letterToBit (unsigned char c, int* bits)
+{
+    int nits[8] = {128, 64, 32, 16, 8, 4, 2, 1};
+}
+
+
+unsigned char* bit_encrypt(const char* text)
+{
+    /*
+    Hello world!
+    change to bit
+
+    H = 72
+    128, 64, 32, 16, 8, 4, 2, 1
+
+    01001000
+    split 4 and 4
+    0100  1000
+    change numbers in 1st group
+    1000
+    XOR 1000
+        1000 1st group after change
+        0000
+
+    write together 1st group after change and resut of XOR
+    10000000
+
+    */
+   
+}
